05ee3afd3968a2d5e16bfb4a12ad2dd5014cb1ed
Annie Chen <anniebh30@gmail.com>
2015-12-29 00:29:26 +0800

    Q1: find on board day

--- spec/working_day_calculator_spec.rb
+++ spec/working_day_calculator_spec.rb
@@ -1,4 +1,18 @@
 require_relative '../working_day_calculator'
 
 RSpec.describe WorkingDayCalculator do
+  let(:holiday) { WorkingDayCalculator.new(Date.new(2016,10,10)) }
+  let(:work_day) { WorkingDayCalculator.new(Date.new(2016,1,5)) }
+  
+  describe 'Q1: find on board day' do  
+    it 'when holiday choose another on board day' do
+      expect(holiday.is_holiday?).to be_truthy
+      expect(holiday.set_on_board_day).to eq('2016-10-11')
+    end
+
+    it 'when work_day just choose the day on board' do
+      expect(work_day.is_holiday?).not_to be_truthy
+      expect(work_day.set_on_board_day).to eq('2016-01-05')
+    end
+  end
 end
--- working_day_calculator.rb
+++ working_day_calculator.rb
@@ -1,2 +1,39 @@
+require 'Date'
+
 class WorkingDayCalculator
+  attr_accessor :date
+
+  def initialize(date_object)
+    @date = date_object
+  end
+
+  def is_holiday?
+    holiday? @date
+  end
+
+  def set_on_board_day
+    while holiday? @date
+      @date += 1
+    end
+    return @date.to_s
+  end
+
+  private
+
+  def holiday? date
+    holidays = [
+      '2016-01-01',
+      '2016-02-08',
+      '2016-02-09',
+      '2016-02-10',
+      '2016-02-11',
+      '2016-02-12',
+      '2016-02-29',
+      '2016-04-05',
+      '2016-06-10',
+      '2016-09-16',
+      '2016-10-10'
+    ]
+    date.saturday? || date.sunday? || holidays.include?(date.to_s)
+  end
 end
